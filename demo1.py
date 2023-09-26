e50f0ffcabf7e81a3b39f0b27f91f523815658cc
Danish-04 <danishmuhammad118@gmail.com>
2023-09-26 14:53:42 +0530

    ligin using while loop

--- demo1.py
+++ demo1.py
@@ -5,30 +5,29 @@ n=0
 def main():
     global n
     
-    username=input("Enter username :")
-    password=input("Enter password :")
-    
-    
-    if username!="Danishkhan" or password!="Danish123":
-        n=n+1
-        if n<3:
-            print("please Try Again..!")
-            main()
-        else:
-            print("sorry..! you have reached maximum attempts")
+    while(n<3):
+        username=input("Enter username :")
+        password=input("Enter password :")
         
     
-        
-    else:      
-        otp=random.randint(1111,9999) 
-        print("Your otp is : ",otp)
-    
-        Enter_otp=int(input("Enter otp :"))
-        
-        if otp == Enter_otp:
-            print("Login successfull..!")
+        if username!="Danishkhan" or password!="Danish123":
             
-        else:
-            print("Incorrect OTP , Please try again...!")
+            print("please Try Again..!")
+            # main()
+            n=n+1   
+            if n==3:
+                print("sorry..! you have reached maximum attempts")
+        
+        else:      
+            otp=random.randint(1111,9999) 
+            print("Your otp is : ",otp)
+        
+            Enter_otp=int(input("Enter otp :"))
+            
+            if otp == Enter_otp:
+                print("Login successfull..!")
+                break
+            else:
+                print("Incorrect OTP , Please try again...!")
         
 main()
